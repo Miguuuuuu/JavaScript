8240dcfcc9ea08c779511b2bdd4199260dafec25
Miguuuuuu <miguelsoaresvlogsff@gmail.com>
2025-05-22 14:44:02 -0300

    let

---
+++ Aula 04/tests.js
@@ -0,0 +1,20 @@
+let name; // Declarou a variával
+name = 'Migu'; // Inicializando a variável
+console.log(name);
+name = 'João' // Apartir daqui a variável muda
+console.log(name);
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número (1nome) , mas você pode coloca número apenas assim: nomecliente1
+// Não podem conter espaços(Nome Cliente) ou traços(-)
+// Utilizamos camelCase
+let nomeCompletodoCliente = 'Miguel Soares';
+console.log(nomeCompletodoCliente);
+// Case-sensitive
+let nomeCliente = 'Luiz';
+nomecliente = 'Otávio';
+
+console.log(nomeCliente, nomecliente)
+// Não podemos redeclarar variáveis com let
+// Não utilize VAR, utilize LET.
