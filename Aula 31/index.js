854b1d0e050f7cba667ed1e4aa0b61049e874f01
Miguuuuuu <miguelsoaresvlogsff@gmail.com>
2025-07-03 17:27:38 -0300

    Var vs let

---
+++ Aula 31/index.js
@@ -0,0 +1,41 @@
+const verdadeira = true;
+
+// Let tem escopo de bloco {... bloco }
+// Var so tem escopo de função
+
+/* let nome = 'Miguel'; // criando
+var nome2 = 'Miguel'; // criando
+
+
+if (verdadeira) {
+    let nome = 'Soares'; // criando
+    var nome2 = 'Rogério'; // Redeclarando
+
+    if (verdadeira) {
+        var nome2 = 'Ronaldo'; // Redeclarada
+        let nome = 'Outra coisa';
+    }
+}
+
+console.log(nome, nome2); */
+
+
+/* function falaOi() {
+
+    if (verdadeira) {
+        let nome = 'Miguel';
+        var sobrenome = 'Soares';
+    }
+
+     console.log(sobrenome);
+}
+
+
+falaOi(); */
+
+console.log(sobrenome); // vai retornar undefined
+
+
+
+
+var sobrenome = 'Soares';
